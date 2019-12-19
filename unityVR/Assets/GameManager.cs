78d74b1d3ef94db788b7641eeb95e698d690ef73
asd0204 <55218578+asd0204@users.noreply.github.com>
2019-12-19 10:54:44 +0800

    1219

---
+++ unityVR/Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static bool havekey;
+
+    public void ThrowPickUP(GameObject obj)
+    {
+        print("撿起物件");
+        havekey = true;
+        Destroy(obj);
+    }
+    public void ThrowDetach()
+    {
+        print("放開物件");
+    }
+    public void ThrowHeld()
+    {
+        print("拿著物件");
+    }
+}
---
+++ unityVR/Assets/TeleportInteratble.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TeleportInteratble : MonoBehaviour {
+
+    public Animator AnimatorControl;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name== "HeadCollider"&&GameManager.havekey)
+        {
+            AnimatorControl.SetBool("character_nearby", true);
+        }
+    }
+}
